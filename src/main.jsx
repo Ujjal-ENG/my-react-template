77c53bd3cf86f9cd918ed49a36e3387a7f942d47
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-07 00:34:09 +0600

    change some portion and now its fully optimized

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import "./index.css";
-import { router } from "./components/routes/Routes.app";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './components/routes/Routes.app';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
